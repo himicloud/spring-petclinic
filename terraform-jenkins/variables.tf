5561f62dd0616fc38880444054f01ff4c4d39780
Himanshu Chaudhary <himanshu.44144+github@gmail.com>
2024-11-04 18:38:22 +1000

    Clean up .gitignore for Terraform files

--- terraform-jenkins/variables.tf
+++ terraform-jenkins/variables.tf
@@ -8,5 +8,16 @@ variable "ami_id" {
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  description = "Instance type for Jenkins"
+  default     = "t2.medium"
+}
+
+variable "snapshot_id" {
+  description = "EBS Snapshot ID for the root volume"
+  default     = "snap-0cadd1c5e99c40055"
+}
+
+variable "volume_size" {
+  description = "Size of the root EBS volume in GB"
+  default     = 20
 }
